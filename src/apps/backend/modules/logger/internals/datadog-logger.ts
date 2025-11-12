fbe8b0704c65c0826cb34ac926748b90782a5884
joeldsouza28 <joel.dsouza@bettrhq.com>
2025-11-13 02:20:04 +0530

    fix: addressed review comments

--- src/apps/backend/modules/logger/internals/datadog-logger.ts
+++ src/apps/backend/modules/logger/internals/datadog-logger.ts
@@ -5,7 +5,9 @@ import * as winston from 'winston';
 export default class DatadogLogger extends WinstonLogger {
   constructor() {
     const DD_REGION = ConfigService.getValue<string>('datadog.region');
-    const DD_API_KEY = ConfigService.getValue<string>('datadog.api_key');
+    const DD_API_KEY = ConfigService.getValue<string>(
+      '/etc/config/datadog.api_key'
+    );
     const DD_APP_NAME = ConfigService.getValue<string>('datadog.app_name');
 
     const transport = new winston.transports.Http({
