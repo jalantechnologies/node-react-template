3fdd40c249f863ed36a6c57d287fb94c14e8b018
joeldsouza28 <joel.dsouza@bettrhq.com>
2025-11-13 14:02:53 +0530

    added fixes

--- src/apps/backend/app.ts
+++ src/apps/backend/app.ts
@@ -1,5 +1,4 @@
 import 'module-alias/register';
-import fs from 'fs';
 import { Server } from 'http';
 import * as path from 'path';
 
@@ -24,17 +23,6 @@ interface APIMicroserviceService {
 }
 
 const isDevEnv = process.env.NODE_ENV === 'development';
-
-const secretsDir = process.env.SECRETS_DIR || '/opt/app/secrets';
-
-fs.readdirSync(secretsDir).forEach((file) => {
-  if (file.startsWith('.')) return;
-
-  const fullPath = path.join(secretsDir, file);
-  if (!fs.statSync(fullPath).isFile()) return;
-
-  process.env[file] = fs.readFileSync(fullPath, 'utf8').trim();
-});
 
 export default class App {
   public static baseAPIRoutePath = '/api';
