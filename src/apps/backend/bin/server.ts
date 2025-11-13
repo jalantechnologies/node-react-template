d83e0c6c1c998746f9f0b7b4daef3e56c02a9f24
joeldsouza28 <joel.dsouza@bettrhq.com>
2025-11-13 14:15:50 +0530

    added fixes

--- src/apps/backend/bin/server.ts
+++ src/apps/backend/bin/server.ts
@@ -1,21 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-import fs from 'fs';
-import path from 'path';
-
 import App from '../app';
-
-const secretsDir = process.env.SECRETS_DIR || '/opt/app/secrets';
-
-fs.readdirSync(secretsDir).forEach((file) => {
-  if (file.startsWith('.')) return;
-
-  const fullPath = path.join(secretsDir, file);
-  if (!fs.statSync(fullPath).isFile()) return;
-
-  process.env[file] = fs.readFileSync(fullPath, 'utf8').trim();
-});
 
 App.startServer().catch((err) => {
   console.error(err);
