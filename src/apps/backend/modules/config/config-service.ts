f90e295cd5e66f1fb1f8dae52f9a949a371c15c6
joeldsouza28 <joel.dsouza@bettrhq.com>
2025-11-19 19:29:00 +0530

    fix: remove patch from scan step

--- src/apps/backend/modules/config/config-service.ts
+++ src/apps/backend/modules/config/config-service.ts
@@ -1,15 +1,22 @@
 import { ConfigMissingError } from 'backend/modules/config';
+import SecretFileLoader from 'backend/modules/config/internals/secret-file-loader';
 import config from 'config';
 import _ from 'lodash';
 
 export default class ConfigService {
   static getValue<T = unknown>(key: string): T {
+    try {
+      const secretOverride = SecretFileLoader.getValue(key) as T;
+      if (!_.isNil(secretOverride)) {
+        return secretOverride;
+      }
+    } catch (error) {
+      console.log(error);
+    }
     const value = config.get<T>(key);
-
     if (_.isNil(value)) {
       throw new ConfigMissingError(key);
     }
-
     return value;
   }
 
